0ba84ac2cf6b09097addf5d53b1453d37a4c7ce3
Chris Crutchfield <ccrutchf@ucsd.edu>
2020-04-01 12:25:18 -0700

    initial checkin of frontend

---
+++ frontend/TvCv19.Frontend/ClientApp/src/app/video-receiver/video-receiver.component.ts
@@ -0,0 +1,27 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+import DailyIframe from '@daily-co/daily-js';
+
+@Component({
+  selector: 'app-video-receiver',
+  templateUrl: './video-receiver.component.html',
+  styleUrls: ['./video-receiver.component.scss']
+})
+export class VideoReceiverComponent implements AfterViewInit {
+
+  @ViewChild('videoFrame') iframeView: ElementRef;
+
+  constructor() { }
+
+  async ngAfterViewInit(): Promise<void> {
+    const callFrame = DailyIframe.wrap(this.iframeView.nativeElement);
+    await callFrame.join({ url: 'https://tvcv19.daily.co/hello' });
+
+    // Since we are the viewer, automatically disable the video.
+    // We will hide the controls so that the volunteer cannot enable video.
+    callFrame
+      .setLocalAudio(false)
+      .setLocalVideo(false);
+  }
+
+}
