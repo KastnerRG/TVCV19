064728958bbc0dfcb580b90c739987a70cb9dd0c
Chris Crutchfield <ccrutchf@ucsd.edu>
2020-04-03 01:12:28 -0700

    chat ui

--- frontend/TvCv19.Frontend/ClientApp/src/app/app.module.ts
+++ frontend/TvCv19.Frontend/ClientApp/src/app/app.module.ts
@@ -2,25 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import  {MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
+import { LineChartModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VolunteerComponent } from './volunteer/volunteer.component';
 import { VideoReceiverComponent } from './video-receiver/video-receiver.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VolunteerComponent,
-    VideoReceiverComponent
+    VideoReceiverComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule
+    LineChartModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
