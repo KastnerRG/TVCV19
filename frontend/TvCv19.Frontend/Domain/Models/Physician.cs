8b7b0473a04b65f484831076ab7efe9acc1870f2
chidi onyema <chidi.onyema@contracted.pmi.com>
2020-04-05 19:59:19 +0000

    added physician repository
    added angular model component and service for physican

---
+++ frontend/TvCv19.Frontend/Domain/Models/Physician.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TvCv19.Frontend.Domain
+{
+   
+    public class Physician
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string CaregiverId { get; set; }
+        public string Location { get; set; }
+        public HierarchyLevel HierarchyLevel { get; set; }
+
+    }
+    public enum HierarchyLevel { FirstLine, Coordinator }
+}
