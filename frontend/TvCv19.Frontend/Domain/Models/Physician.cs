a2fe35c96052b696d9e3a4553f11f0ff5f1cad1a
Chris Crutchfield <ccrutchf@ucsd.edu>
2020-04-05 12:51:21 -0700

    physician repository

---
+++ frontend/TvCv19.Frontend/Domain/Models/Physician.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TvCv19.Frontend.Domain.Models
+{
+    public class Physician
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public List<Physician> Supervisees { get; set; }
+    }
+}
---
+++ frontend/TvCv19.Frontend/Domain/Repositories/IPhysicianRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TvCv19.Frontend.Domain.Models;
+
+namespace TvCv19.Frontend.Domain.Repositories
+{
+    public interface IPhysicianRepository
+    {
+        Task<string> AddPhysicianAsync(Physician physician);
+        Task DeletePhysicianAsync(string id);
+        Task<IEnumerable<Physician>> GetPhysiciansAsync();
+        Task<Physician> GetPhysicianAsync(string id);
+        Task UpdatePhysicianAsync(Physician physician);
+    }
+
+    public class PocPhyscianRepository : IPhysicianRepository
+    {
+        private static HashSet<Physician> _physicians = new HashSet<Physician>();
+
+        public Task<string> AddPhysicianAsync(Physician physician)
+        {
+            _physicians.Add(physician);
+
+            return Task.FromResult(physician.Id);
+        }
+
+        public async Task DeletePhysicianAsync(string id)
+        {
+            var physicianToDelete = await GetPhysicianAsync(id);
+            _physicians.Remove(physicianToDelete);
+        }
+
+        public Task<IEnumerable<Physician>> GetPhysiciansAsync() => Task.FromResult((IEnumerable<Physician>)_physicians);
+
+        public Task<Physician> GetPhysicianAsync(string id) => Task.FromResult(_physicians.FirstOrDefault(x => x.Id == id));
+
+        public async Task UpdatePhysicianAsync(Physician physician)
+        {
+            await DeletePhysicianAsync(physician.Id);
+            await AddPhysicianAsync(physician);
+        }
+    }
+}
