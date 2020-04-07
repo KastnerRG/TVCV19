d9e06c20870a8e76021db064ea167e5f069ab564
Michael Barrow <mbarrow@eng.ucsd.edu>
2020-04-07 14:51:59 -0700

    added constructor

--- ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
+++ ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
@@ -17,7 +17,7 @@
 //Constant control strings. Meet specification but do not require complex logic.
 
 //Ventilator Sensor Names
-//Control Knobs
+//Control Knobs (CONTROL REGISTERS)
 #define RespiratoryRate                 "RrRt"
 #define TidalVolume                     "TlVm"
 #define MaximumInspiratoryPressure      "MmIP"
@@ -25,14 +25,16 @@
 #define InspiratoryTimeExpiratoryTime   "ITET"
 #define FiO2                            "FiO2"
 
-//Measured Variables for decision making
+//Measured Variables for decision making (STATUS REGISTERS)
 #define MinuteVentilation               "MtVn"
 #define PeakInspiratoryPressure         "PkIP"
 #define PCO2                            "PCO2"
 
+//TODO: Define global variables with appropriate mmio address locations 
+
 
 //Command String Constants
-const char 		  Ack 		 = 'p';			 	//Ack character to send (should really replace with 'ACK')
+const char 		  Ack 		 = 6;			 	//Ack character to send (should really replace with 'ACK')
 const char* const OpenChlAck = "{\"1\":{}}";	//Send bus capabilities during handshake (version 1, no vendor extensions).
 const char* const FullRead   =                  //Read All Sensors at once.
 "{\"rd\":[\
@@ -57,6 +59,29 @@ typedef struct POLICY
 	
 }policy;
 
+//See also "CONTROL REGISTERS" for verbose names of contol registers.
+typedef struct _CONTROL
+{
+    short rrrt; //RespiratoryRate
+    short tlvm; //TidalVolume
+    short mmip; //MaximumInspiratoryPressure
+    short pkep; //PeakEndExpiratoryPressure
+    short itet; //InspiratoryTimeExpiratoryTime
+    short fio2; //FiO2
+    //TODO: Initialize to appropriate values
+    _CONTROL(){rrrt=0; tlvm=0; mmip=0; pkep=0; itet=0; fio2=0;}
+}control;
+
+//See also "STATUS REGISTERS" 
+typedef struct _STATUS
+{
+    short mtvn; // MinuteVentilation               "MtVn"
+    short pkip; // PeakInspiratoryPressure         "PkIP"
+    short pco2; // PCO2                            "PCO2"
+    //TODO: Initialize to appropriate values
+    _STATUS(){mtvn=0; pkip=0; pco2=0;}
+}status;
+
 /*===========================================================================*\
 |				VENTILATOR CONTROL API FUNCTION SIGNATURES	  				  |		
 \*===========================================================================*/
@@ -69,14 +94,17 @@ class AudioIO
   public:
     AudioIO();
     void begin();           //initialize comm channel
-    void sendVentilatorData(word pco2);
+    void sendVentilatorData(short pco2);
     void doSomething(void);
 
   // library-accessible "private" interface
   private:
-
-    policy _mPolicyState;
-	//void doSomethingSecret(void);
+    
+    policy _mPolicyState;   //bus policy state
+    control _mControlRegs;  //appropriately mapped control registers
+	status _mStatusRegs;    //appropriately mapped status registers. Read to get ventilator info
+    
+    //void doSomethingSecret(void);
 };
 
 #endif
