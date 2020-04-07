ab24bbe77028323ee63608de3ef3fd054af02623
Michael Barrow <mbarrow@eng.ucsd.edu>
2020-04-07 02:37:02 -0700

    sensor names and command string format example in AudioIO.h

--- ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
+++ ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
@@ -7,9 +7,41 @@
 #define AudioIO_h
 
 // include types & constants of Wiring core API
-//#include "WConstants.h"
 #include <Arduino.h>
 #include <SoftModem.h>
+
+/*===========================================================================*\
+|	CONTROL STRINGS FOR COMPLIANCE WITH VENTILATOR API SPECIFICATION Ver 1	  |		
+\*===========================================================================*/
+
+//Constant control strings. Meet specification but do not require complex logic.
+
+//Ventilator Sensor Names
+//Control Knobs
+#define RespiratoryRate                 "RrRt"
+#define TidalVolume                     "TlVm"
+#define MaximumInspiratoryPressure      "MmIP"
+#define PeakEndExpiratoryPressure       "PkEP"
+#define InspiratoryTimeExpiratoryTime   "ITET"
+#define FiO2                            "FiO2"
+
+//Measured Variables for decision making
+#define MinuteVentilation               "MtVn"
+#define PeakInspiratoryPressure         "PkIP"
+#define PCO2                            "PCO2"
+
+
+//Command String Constants
+const char 		  Ack 		 = 'p';			 	//Ack character to send (should really replace with 'ACK')
+const char* const OpenChlAck = "{\"1\":{}}";	//Send bus capabilities during handshake (version 1, no vendor extensions).
+const char* const FullRead   =                  //Read All Sensors at once.
+"{\"rd\":[\
+	{\"" MinuteVentilation "\":%i}\
+    {\"" PeakInspiratoryPressure "\":%i}\
+    {\"" PCO2 "\":%i}\	        
+]}";
+
+
 
 enum{PCM,IO}busMode;
 
@@ -25,13 +57,19 @@ typedef struct POLICY
 	
 }policy;
 
+/*===========================================================================*\
+|				VENTILATOR CONTROL API FUNCTION SIGNATURES	  				  |		
+\*===========================================================================*/
+
+
 // library interface description
 class AudioIO
 {
   // user-accessible "public" interface
   public:
     AudioIO();
-    void begin();
+    void begin();           //initialize comm channel
+    void sendVentilatorData(word pco2);
     void doSomething(void);
 
   // library-accessible "private" interface
