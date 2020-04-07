9c9919648dee0ee6a7982c7bec4e0871109e6d3a
Michael Barrow <mbarrow@eng.ucsd.edu>
2020-04-06 22:51:51 -0700

    template library and demo sketch of 'AudioIO' for ventilator control

---
+++ ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/sample_AudioIO/sample_AudioIO.ino
@@ -0,0 +1,50 @@
+#include <AudioIO.h>
+
+AudioIO io = AudioIO();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  //delay(100);
+  io.begin();
+  //modem.begin();
+}
+
+void loop() 
+{ 
+  //TODO: A demo of the Audio IO functions
+
+   
+  //Only write out.
+  /*Serial.println("Testing output");
+  while(1)
+  {
+    modem.write('b');
+    modem.write('a');
+    modem.write('d');
+    modem.write(' ');
+    //modem.write('\0');
+  }*/
+
+  /*
+  //loop back
+  while(modem.available()){
+    int c = modem.read();
+    if(isprint(c)){
+      Serial.print((char)c);
+    }
+    else{
+      Serial.print("(");
+      Serial.print(c,HEX);
+      Serial.println(")");      
+    }
+  }
+  if(Serial.available()){
+    modem.write(0xff);
+    while(Serial.available()){
+      char c = Serial.read();
+      modem.write(c);
+    }
+  }
+  */
+}
