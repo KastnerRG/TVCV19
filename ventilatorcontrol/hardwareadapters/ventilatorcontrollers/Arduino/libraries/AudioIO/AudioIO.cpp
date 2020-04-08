14e1dd03fd533da4384146a4f8fe5868dfabde64
Michael Barrow <mbarrow@eng.ucsd.edu>
2020-04-07 19:26:32 -0700

    updated class template in library

--- ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.cpp
+++ ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.cpp
@@ -18,12 +18,11 @@ AudioIO::AudioIO(){}	//Don't do anything. follow the pattern of SoftModem();
 
 void AudioIO::begin()
 {
-
   // initialize this instance's variables
 
   // do whatever is required to initialize the library
   modem = SoftModem();	//Will be used to communicate with bus master.
-
+  
   Serial.println("AudioIO startup");
   delay(100);
   modem.begin();
@@ -32,30 +31,80 @@ void AudioIO::begin()
 // Public Methods //////////////////////////////////////////////////////////////
 // Functions available in Wiring sketches, this library, and other libraries
 
-
-
-void AudioIO::doSomething(void)
+//Function to parse a command that lives in the _mMasterData member
+//TODO make this private and make it only exist in a library instantiated loop.
+void AudioIO::parseCommand()
 {
-  /*
-  // eventhough this function is public, it can access
-  // and modify this library's private variables
-  Serial.print("value is ");
-  Serial.println(value);
-  */
-
-  // it can also call private functions of this library
-  //doSomethingSecret();
+    //check we have data
+    if(_mMasterData.available)
+    {
+        //Clear command regardless.
+        _mMasterData.available = false;
+        
+        //Process command. Very basic. Only consider the 4th character in the buffer.
+        switch(_mMasterData.buffer_cmd[3])
+        {
+            case 'd':   //Assume master wants to read ventilator data (rd) string
+                sendVentilatorData();
+                break;
+                
+            case 'k':   //Assume master wants to write control knobs
+                setVentilatorControl();
+                break;
+                
+            default: 
+                Serial.println("Warning! Unrecognized Master command:");//We don't handle this command (maybe we should)
+                Serial.println(_mMasterData.buffer_cmd);
+                break;
+        }
+    }
 }
 
 // Private Methods /////////////////////////////////////////////////////////////
 // Functions only available to other functions in this library
 
-/*
-void AudioIO::doSomethingSecret(void)
+void AudioIO::sendVentilatorData()
 {
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(200);
+    //DEBUG ONLY, WRITE A GENERIC STRING TO THE UART
+    //Return the internal copies of class struct
+    unsigned char i = 0;
+    
+    while(i<FULLREADLEN-(3*2)//DEBUG!!!!!
+            )
+    {
+        Serial.print(*(FullRead+i));
+        i++;
+    }
+    Serial.print('\n');
 }
-*/
+
+void AudioIO::setVentilatorControl()
+{
+    //DEBUG ONLY, WRITE A GENERIC STRING TO THE UART
+    //Set the internal control word of class struct
+    unsigned char i = 0;    //counter
+    
+    char val[4];
+    
+    nonNullShortitoa(56, val);      //convert ventilator readout to ascii
+    
+    //DEBUG! check the offsets are the correct length
+    while(i<REPORTKNOBSLEN-(6*2)//DEBUG!!!!!
+            )
+    {
+        Serial.print(*(ReportKnobs+i));
+        i++;
+    }
+    Serial.print('\n');
+}
+
+//utility members
+void AudioIO::nonNullShortitoa(short s, char* a)
+{
+    short rem;
+    a[0]='0';a[1]='0';a[2]='0';a[3]='0';
+    while (s != 0) 
+    { 
+        rem = s % 10; 
+    }
+}
--- ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
+++ ventilatorcontrol/hardwareadapters/ventilatorcontrollers/Arduino/libraries/AudioIO/AudioIO.h
@@ -30,6 +30,11 @@
 #define PeakInspiratoryPressure         "PkIP"
 #define PCO2                            "PCO2"
 
+//Bus management related
+#define BUFFERSIZE 100
+#define FULLREADLEN 50
+#define REPORTKNOBSLEN 92
+
 //TODO: Define global variables with appropriate mmio address locations
 
 
@@ -38,11 +43,20 @@ const char 		  Ack 		 = 6;			 	//Ack character to send (should really replace wi
 const char* const OpenChlAck = "{\"1\":{}}";	//Send bus capabilities during handshake (version 1, no vendor extensions).
 const char* const FullRead   =                  //Read All Sensors at once.
 "{\"rd\":[\
-	{\"" MinuteVentilation "\":%i}\
-    {\"" PeakInspiratoryPressure "\":%i}\
-    {\"" PCO2 "\":%i}\
-]}";
-
+{\"" MinuteVentilation "\":%i},\
+{\"" PeakInspiratoryPressure "\":%i},\
+{\"" PCO2 "\":%i}\
+]}";                                            //52 chars(including null)
+const char* const WriteKnobs =   "{\"rk\":[";    //Look for this key part of the command in order to write control knobs.
+const char* const ReportKnobs =                 //Report back all control knob values
+"{\"rk\":[\
+{\"" RespiratoryRate "\":%i},\
+{\"" TidalVolume "\":%i},\                   
+{\"" MaximumInspiratoryPressure "\":%i},\
+{\"" PeakEndExpiratoryPressure "\":%i},\     
+{\"" InspiratoryTimeExpiratoryTime "\":%i},\     
+{\"" FiO2 "\":%i}\
+]}";                                            //94 chars (including null)
 
 
 enum{PCM,IO}busMode;
@@ -82,6 +96,12 @@ typedef struct _STATUS
     _STATUS(){mtvn=0; pkip=0; pco2=0;}
 }status;
 
+typedef struct _INCOMINGDATA
+{
+    char buffer_cmd[BUFFERSIZE];  //Data found by the slave bus controller
+    bool available;               //Set when new data is found.
+}incomingdata;
+
 /*===========================================================================*\
 |				VENTILATOR CONTROL API FUNCTION SIGNATURES	  				  |
 \*===========================================================================*/
@@ -94,18 +114,27 @@ class AudioIO
   public:
     AudioIO();
     void begin();           //initialize comm channel
-    void sendVentilatorData(short pco2);
-    void doSomething(void);
+        
+    //TODO: Make private and have ARTe loop start up in the 
+    void parseCommand();        //check _mMasterData and process it as makes sense
 
+    
+    void sendVentilatorData();      //Send ventilator data to the bus master
+    void setVentilatorControl();    //Set control register values using data from bus master
+    
   // library-accessible "private" interface
   private:
 
-		SoftModem modem;
-		policy _mPolicyState;   //bus policy state
-    control _mControlRegs;  //appropriately mapped control registers
-	status _mStatusRegs;    //appropriately mapped status registers. Read to get ventilator info
-
-    //void doSomethingSecret(void);
+    void nonNullShortitoa(short s, char* a);   //very basic itoa that does not null terminate, instead has leading 0's
+    
+    SoftModem       modem;         //Used for IO
+    incomingdata    _mMasterData;  //Data from the bus master
+    policy          _mPolicyState; //bus policy state
+    control         _mControlRegs; //appropriately mapped control registers
+	status          _mStatusRegs;  //appropriately mapped status registers. Read to get ventilator info
+	
+//	void sendVentilatorData();      //Send ventilator data to the bus master
+//	void setVentilatorControl();    //Set control register values using data from bus master
 };
 
 #endif
