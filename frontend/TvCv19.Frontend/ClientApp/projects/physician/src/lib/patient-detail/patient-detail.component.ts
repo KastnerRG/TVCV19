b70029a7c450e90a563f94252cbf4de8f2248c2d
stephen-barrow <38037209+stephen-barrow@users.noreply.github.com>
2020-04-11 19:10:48 +0100

    Styled as close to mock added name and date to the message (#10)

--- frontend/TvCv19.Frontend/ClientApp/projects/physician/src/lib/patient-detail/patient-detail.component.ts
+++ frontend/TvCv19.Frontend/ClientApp/projects/physician/src/lib/patient-detail/patient-detail.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'lib-patient-detail',
   templateUrl: './patient-detail.component.html',
-  styleUrls: ['./patient-detail.component.scss']
+  styleUrls: ['./patient-detail.component.scss'],
 })
 export class PatientDetailComponent implements OnInit {
   patientId: any;
+  physicianId: any;
 
-  constructor(route: ActivatedRoute) {
-  route.params.subscribe(p => 
-     this.patientId = p['patient-id'])
+  constructor(route: ActivatedRoute, private router: Router) {
+    route.params.subscribe((p) => (this.patientId = p['patient-id']));
+    route.parent.params.subscribe((p: any) => {
+      this.physicianId = p['physician-id']
+    })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  chat(): void {
+    this.router.navigateByUrl(`/physician/${this.physicianId}/patient/${this.patientId}/chat`);
+  }
 }
