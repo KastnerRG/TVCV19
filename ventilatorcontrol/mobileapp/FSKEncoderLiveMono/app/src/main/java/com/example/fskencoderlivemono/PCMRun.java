0877705ead137c3dc0b796175235852c48b9de4d
Michael Barrow <mbarrow@eng.ucsd.edu>
2020-04-21 23:24:43 -0700

    broken wave

--- ventilatorcontrol/mobileapp/FSKEncoderLiveMono/app/src/main/java/com/example/fskencoderlivemono/PCMRun.java
+++ ventilatorcontrol/mobileapp/FSKEncoderLiveMono/app/src/main/java/com/example/fskencoderlivemono/PCMRun.java
@@ -21,10 +21,47 @@ class PCMRun implements Runnable {
         float samples[] = new float[1024];
         while (true) {
             //Frequency range is [0-100] corresponding to frequencies 0 - ~4KHz
-            float increment = (float) (2 * Math.PI) * this.freq_level * 215 / 44100;
+            /*float increment = (float) (2 * Math.PI) * this.freq_level * 215 / 44100;
             for (int i = 0; i < samples.length; i++) {
                 samples[i] = (float) (Math.sin(angle));
                 angle += increment;
+            }*/
+            //float increment = (float) (2 * Math.PI) * this.freq_level * 215 / 44100;
+            int j = (int)this.freq_level/2;
+            //int k = 50;
+            int max = 100;
+            int n = 0;
+            for (int i = 0; i < samples.length; i++) {
+                if(n++ < max/2)
+                {
+                    samples[i] = 1;
+                }
+                else
+                {
+                    samples[i] = 0;
+                }
+                if(n == max)
+                {
+                    n = 0;
+                }
+
+                //for(int j = 0; j<100; j++)
+                /*
+                if(j-- > 0)
+                    samples[i] = 1;//(float) (Math.sin(angle));
+                 else if(k-- > 0)
+                 {
+                     samples[i]= 0;
+
+                 }
+                 else if(k++ < (50-(int)this.freq_level/2))
+                 {
+                     samples[i]=0;
+                     k = 50;
+                     j= (int)this.freq_level/2;
+                 }*/
+
+                //angle += increment;
             }
             device.writeSamples(samples);
         }
