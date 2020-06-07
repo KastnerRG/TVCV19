52e098289caaa60fae38e1ce4480f6162b638633
Jeremy Ford <>
2020-06-07 12:01:04 -0700

    Added patient-qr functionality and styling

--- frontend/TvCv19.Frontend/ClientApp/projects/patient/src/lib/patient-qr/patient-qr.component.ts
+++ frontend/TvCv19.Frontend/ClientApp/projects/patient/src/lib/patient-qr/patient-qr.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToolbarService } from 'src/app/toolbar.service';
+import { PatientModel, PatientService } from 'projects/shared/src/public-api';
 
 @Component({
   selector: 'lib-patient-qr',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-qr.component.css']
 })
 export class PatientQrComponent implements OnInit {
+  id: string;
+  patient: PatientModel;
 
-  constructor() { }
+  constructor(route: ActivatedRoute, private toolbar: ToolbarService, private service: PatientService) {
+    route.params.subscribe((p) => {
+      this.id = p['id'];
+    });
+  }
 
   ngOnInit(): void {
+    this.service.getPatient(this.id).subscribe((p) => {
+      this.patient = p;
+    });
+
+    this.toolbar.show();
+    this.toolbar.setToolbarData({
+      back: true
+    });
   }
 
 }
