fb9ce662bdbc4c586605b11382791f983cb80739
Jeremy Ford <>
2020-06-07 12:00:34 -0700

    Fixed patient-menu toggle, added styling

--- frontend/TvCv19.Frontend/ClientApp/projects/patient/src/lib/patient-feed/patient-menu/patient-menu.component.ts
+++ frontend/TvCv19.Frontend/ClientApp/projects/patient/src/lib/patient-feed/patient-menu/patient-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { PatientModel, PatientService } from 'projects/shared/src/public-api';
+import { PatientFeedComponent } from '../patient-feed.component'
 
 @Component({
   selector: 'lib-patient-menu',
@@ -8,16 +9,20 @@ import { PatientModel, PatientService } from 'projects/shared/src/public-api';
   styleUrls: ['./patient-menu.component.css']
 })
 export class PatientMenuComponent implements OnInit {
-  @Input() showMenu: boolean;
+  @Input() menuVisible: boolean;
   @Input() patientID: string;
   patient: PatientModel;
 
-  constructor(private route: ActivatedRoute, private service: PatientService) { }
+  constructor(private route: ActivatedRoute, private service: PatientService, private patientFeed: PatientFeedComponent) { }
   
   ngOnInit(): void {
     this.service.getPatient(this.patientID).subscribe((p) => {
-      this.patient= p;
+      this.patient = p;
     });
+  }
+
+  toggleMenu() {
+    this.patientFeed.toggleMenu();
   }
   
 }
