95509c45313bd9c3b06278096ca812596368355e
Stephen Barrow <stephen.barrow@decision.tech>
2020-06-12 21:36:42 +0100

    added hierarchy

--- frontend/TvCv19.Frontend/Domain/Repositories/PhysicianRepository.cs
+++ frontend/TvCv19.Frontend/Domain/Repositories/PhysicianRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 using MySql.Data.MySqlClient;
@@ -46,7 +47,7 @@ namespace TvCv19.Frontend.Domain.Repositories
         public async Task<Physician> GetPhysicianAsync(string id)
         {
             var param = new { id };
-            var sql = "SELECT * FROM medecc.caregiver WHERE id = @id";
+            var sql = "SELECT id, name, location, hierarchy, supervisor_id as supervisorId FROM medecc.caregiver WHERE id = @id";
             return await GetFirstOrDefaultAsync<Physician>(sql, param);
         }
 
@@ -61,6 +62,33 @@ namespace TvCv19.Frontend.Domain.Repositories
             return physician;
         }
 
+        public async Task<CarerHierarchyTree> GetHeirarchyTree(string id)
+        {
+            var directReports = await GetPhysicianTeam(id);
+            if(!directReports.Any()) {
+                var supervisorId = (await GetPhysicianAsync(id)).SupervisorId;
+                var supervisor = (await GetPhysiciansAsync()).Where(p => p.Id == supervisorId).FirstOrDefault();
+                return new CarerHierarchyTree() {
+                    Name = "Supervisor",
+                    Children = new [] {new CarerHierarchyTree() {
+                        Name = supervisor.Name
+                    }}
+                };
+            }
+            var tree = new CarerHierarchyTree() {
+                Name = "Care Team"
+            };
+            var children = (await Task.WhenAll(directReports.Select(async r => new CarerHierarchyTree() {
+                Name = r.Name,
+                Hierarchy = r.Hierarchy,
+                Children = (await GetPhysicianTeam(r.Id)).Select(bc => new CarerHierarchyTree() {
+                    Name = bc.Name,
+                    Hierarchy = bc.Hierarchy
+                }) 
+            })));
+            tree.Children = children;
+            return tree;
+        }
     }
 }
 
